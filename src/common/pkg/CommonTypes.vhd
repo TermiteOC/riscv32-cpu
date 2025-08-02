77583084752fa1e377dd87405c932a44a8223ab1
Levy Elmescany <levyelmescany@edu.univali.br>
2025-08-02 20:12:07 -0300

    feat: add CommonTypes package with WordArray type definition

---
+++ src/common/pkg/CommonTypes.vhd
@@ -0,0 +1,10 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+package CommonTypes is
+    -- Shared types
+    type WordArray_t is array(0 to 31) of std_logic_vector(31 downto 0);
+end package;
+
+package body CommonTypes is
+end package body;
