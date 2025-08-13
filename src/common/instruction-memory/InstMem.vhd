732d39e501aed2b70372f01ca886f1a4727f57e5
Levy Elmescany <levyelmescany@edu.univali.br>
2025-08-12 23:38:20 -0300

    feat(instmem): replace zero initialization with basic RARS program including operators supported by the ALU

--- src/common/instruction-memory/InstMem.vhd
+++ src/common/instruction-memory/InstMem.vhd
@@ -15,7 +15,8 @@
 -- Simulation: To be verified via tb_InstMem testbench
 -- Revision History:
 --   Rev 1.0 - 2025-08-11 - Initial implementation
---   Rev 1.1 - 2025-08-12 - Changed address input to receive binary value (std_logic_vector)
+--   Rev 1.1 - 2025-08-12 - Changed address input to receive binary value (std_logic_vector);
+--                          initializing with basic RARS program for simulation purposes
 ------------------------------------------------------------------------------- 
 
 library ieee;
@@ -44,8 +45,22 @@ architecture rtl of InstMem is
     type memory_t is array(2**ADDR_WIDTH-1 downto 0) of word_t;
 
     function init_rom return memory_t is
+        variable temp : memory_t := (others => (others => '0'));
     begin
-        return (others => (others => '0'));
+        temp(0)  := x"00500093"; -- addi x1, x0, 5
+        temp(1)  := x"00A00113"; -- addi x2, x0, 10
+        temp(2)  := x"002081B3"; -- add x3, x1, x2
+        temp(3)  := x"40110233"; -- sub x4, x2, x1
+        temp(4)  := x"0020F2B3"; -- and x5, x1, x2
+        temp(5)  := x"0020E333"; -- or  x6, x1, x2
+        temp(6)  := x"0020A4B3"; -- slt x9, x1, x2
+        temp(7)  := x"00208663"; -- beq x1, x2, 12
+        temp(8)  := x"00100513"; -- addi x10, x0, 1
+        temp(9)  := x"00000663"; -- beq x0, x0, 12
+        temp(10) := x"00200513"; -- addi x10, x0, 2
+        temp(11) := x"00A57593"; -- andi x11, x10, 10
+        temp(12) := x"00000013"; -- addi x0, x0, 0 (NOP)
+    return temp;
     end init_rom;   
 
     -- Declare the ROM signal and specify a default value.
