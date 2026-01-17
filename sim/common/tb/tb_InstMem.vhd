2314908f44cf4fdfc140c3bb058633e54f8211a3
Levy Elmescany <levyelmescany@edu.univali.br>
2026-01-16 23:36:22 -0300

    refactor(instmem): change initialized assembly program

--- sim/common/tb/tb_InstMem.vhd
+++ sim/common/tb/tb_InstMem.vhd
@@ -16,7 +16,8 @@
 -- Revision History:
 --   Rev 1.0 - 2025-08-13 - Initial testbench implementation
 --   Rev 1.1 - 2025-08-14 - Updated testbench to match asynchronous InstMem design;
---                          removed clock dependency and adjusted wait statements.
+--                          removed clock dependency and adjusted wait statements
+--   Rev 1.2 - 2026-01-16 - Changed testbench to reflect new initialized RARS program
 -------------------------------------------------------------------------------
 
 library IEEE;
@@ -65,83 +66,95 @@ begin
     -- Stimulus process
     process
     begin
-        -- Test case 1: Fetch address 0, expect (0x00500093)
+        -- Test case 1: Fetch address 0, expect (0x00500413)
         w_addr <= std_logic_vector(to_unsigned(0, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"00500093"
-        report "Error: Test case 1 - expected output = x'00500093'" severity error;
+        assert w_q = x"00500413"
+        report "Error: Test case 1 - expected output = x'00500413'" severity error;
 
-        -- Test case 2: Fetch address 1, expect (0x00A00113)
+        -- Test case 2: Fetch address 1, expect (0x00a00493)
         w_addr <= std_logic_vector(to_unsigned(1, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"00A00113"
-        report "Error: Test case 2 - expected output = x'00A00113'" severity error;
+        assert w_q = x"00a00493"
+        report "Error: Test case 2 - expected output = x'00a00493'" severity error;
 
-        -- Test case 3: Fetch address 2, expect (0x002081B3)
+        -- Test case 3: Fetch address 2, expect (0x009402b3)
         w_addr <= std_logic_vector(to_unsigned(2, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"002081B3"
-        report "Error: Test case 3 - expected output = x'002081B3'" severity error;
+        assert w_q = x"009402b3"
+        report "Error: Test case 3 - expected output = x'009402b3'" severity error;
 
-        -- Test case 4: Fetch address 3, expect (0x40110233)
+        -- Test case 4: Fetch address 3, expect (0x40848333)
         w_addr <= std_logic_vector(to_unsigned(3, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"40110233"
-        report "Error: Test case 4 - expected output = x'40110233'" severity error;
+        assert w_q = x"40848333"
+        report "Error: Test case 4 - expected output = x'40848333'" severity error;
 
-        -- Test case 5: Fetch address 4, expect (0x0020F2B3)
+        -- Test case 5: Fetch address 4, expect (0x009473b3)
         w_addr <= std_logic_vector(to_unsigned(4, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"0020F2B3"
-        report "Error: Test case 5 - expected output = x'0020F2B3'" severity error;
+        assert w_q = x"009473b3"
+        report "Error: Test case 5 - expected output = x'009473b3'" severity error;
 
-        -- Test case 6: Fetch address 5, expect (0x0020E333)
+        -- Test case 6: Fetch address 5, expect (0x00946e33)
         w_addr <= std_logic_vector(to_unsigned(5, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"0020E333"
-        report "Error: Test case 6 - expected output = x'0020E333'" severity error;
+        assert w_q = x"00946e33"
+        report "Error: Test case 6 - expected output = x'00946e33'" severity error;
 
-        -- Test case 7: Fetch address 6, expect (0x0020A4B3)
+        -- Test case 7: Fetch address 6, expect (0x00942933)
         w_addr <= std_logic_vector(to_unsigned(6, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"0020A4B3"
-        report "Error: Test case 7 - expected output = x'0020A4B3'" severity error;
+        assert w_q = x"00942933"
+        report "Error: Test case 7 - expected output = x'00942933'" severity error;
 
-        -- Test case 8: Fetch address 7, expect (0x00208663)
+        -- Test case 8: Fetch address 7, expect (0x06400993)
         w_addr <= std_logic_vector(to_unsigned(7, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"00208663"
-        report "Error: Test case 8 - expected output = x'00208663'" severity error;
+        assert w_q = x"06400993"
+        report "Error: Test case 8 - expected output = x'06400993'" severity error;
 
-        -- Test case 9: Fetch address 8, expect (0x00100513)
+        -- Test case 9: Fetch address 8, expect (0x00790663)
         w_addr <= std_logic_vector(to_unsigned(8, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"00100513"
-        report "Error: Test case 9 - expected output = x'00100513'" severity error;
+        assert w_q = x"00790663"
+        report "Error: Test case 9 - expected output = x'00790663'" severity error;
 
-        -- Test case 10: Fetch address 9, expect (0x00000663)
+        -- Test case 10: Fetch address 9, expect (0x00100993)
         w_addr <= std_logic_vector(to_unsigned(9, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"00000663"
-        report "Error: Test case 10 - expected output = x'00000663'" severity error;
+        assert w_q = x"00100993"
+        report "Error: Test case 10 - expected output = x'00100993'" severity error;
 
-        -- Test case 11: Fetch address 10, expect (0x00200513)
+        -- Test case 11: Fetch address 10, expect (0x00700463)
         w_addr <= std_logic_vector(to_unsigned(10, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"00200513"
-        report "Error: Test case 11 - expected output = x'00200513'" severity error;
+        assert w_q = x"00700463"
+        report "Error: Test case 11 - expected output = x'00700463'" severity error;
 
-        -- Test case 12: Fetch address 11, expect (0x00A57593)
+        -- Test case 12: Fetch address 11, expect (0x00200993)
         w_addr <= std_logic_vector(to_unsigned(11, TB_ADDR_WIDTH));
         wait for 1 ns;
-        assert w_q = x"00A57593"
-        report "Error: Test case 12 - expected output = x'00A57593'" severity error;
+        assert w_q = x"00200993"
+        report "Error: Test case 12 - expected output = x'00200993'" severity error;
 
-        -- Test case 13: Fetch address 12, expect (0x00000013)
+        -- Test case 13: Fetch address 12, expect (0x0079fe93)
         w_addr <= std_logic_vector(to_unsigned(12, TB_ADDR_WIDTH));
         wait for 1 ns;
+        assert w_q = x"0079fe93"
+        report "Error: Test case 13 - expected output = x'0079fe93'" severity error;
+
+        -- Test case 14: Fetch address 13, expect (0x0049ef13)
+        w_addr <= std_logic_vector(to_unsigned(13, TB_ADDR_WIDTH));
+        wait for 1 ns;
+        assert w_q = x"0049ef13"
+        report "Error: Test case 14 - expected output = x'0049ef13'" severity error;
+
+        -- Test case 15: Fetch address 14, expect (0x00000013)
+        w_addr <= std_logic_vector(to_unsigned(14, TB_ADDR_WIDTH));
+        wait for 1 ns;
         assert w_q = x"00000013"
-        report "Error: Test case 13 - expected output = x'00000013'" severity error;
+        report "Error: Test case 15 - expected output = x'00000013'" severity error;
 
         -- Clear inputs
         w_addr <= (others => '0');
--- src/common/instruction-memory/InstMem.vhd
+++ src/common/instruction-memory/InstMem.vhd
@@ -19,7 +19,9 @@
 --   Rev 1.1 - 2025-08-12 - Changed address input to receive binary value (std_logic_vector);
 --                          initializing with basic RARS program for simulation purposes
 --   Rev 1.2 - 2025-08-14 - Modified to asynchronous ROM: output updates immediately with 
---                          address changes, no clock required;
+--                          address changes, no clock required
+--   Rev 2.0 - 2026-01-16 - Changed initialized RARS assmebly program to simulate every feature
+--                          of the RISCV-32 CPU
 -------------------------------------------------------------------------------
 
 library ieee;
@@ -49,19 +51,21 @@ architecture rtl of InstMem is
     function init_rom return memory_t is
         variable temp : memory_t := (others => (others => '0'));
     begin
-        temp(0)  := x"00500093"; -- addi x1, x0, 5
-        temp(1)  := x"00A00113"; -- addi x2, x0, 10
-        temp(2)  := x"002081B3"; -- add x3, x1, x2
-        temp(3)  := x"40110233"; -- sub x4, x2, x1
-        temp(4)  := x"0020F2B3"; -- and x5, x1, x2
-        temp(5)  := x"0020E333"; -- or  x6, x1, x2
-        temp(6)  := x"0020A4B3"; -- slt x9, x1, x2
-        temp(7)  := x"00208663"; -- beq x1, x2, 12
-        temp(8)  := x"00100513"; -- addi x10, x0, 1
-        temp(9)  := x"00000663"; -- beq x0, x0, 12
-        temp(10) := x"00200513"; -- addi x10, x0, 2
-        temp(11) := x"00A57593"; -- andi x11, x10, 10
-        temp(12) := x"00000013"; -- addi x0, x0, 0 (NOP)
+        temp(0)  := x"00500413"; -- addi x8, x0, 5
+        temp(1)  := x"00a00493"; -- addi x9, x0, 10
+        temp(2)  := x"009402b3"; -- add x5, x8, x9
+        temp(3)  := x"40848333"; -- sub x6, x9, x8
+        temp(4)  := x"009473b3"; -- and x7, x8, x9
+        temp(5)  := x"00946e33"; -- or x28, x8, x9
+        temp(6)  := x"00942933"; -- slt x18, x8, x9
+        temp(7)  := x"06400993"; -- addi x19, x0, 100
+        temp(8)  := x"00790663"; -- beq x18, x7, 0x0000000c
+        temp(9)  := x"00100993"; -- addi x19, x0, 1
+        temp(10) := x"00700463"; -- beq x0, x7, 0x000 00008
+        temp(11) := x"00200993"; -- addi x19, x0, 2
+        temp(12) := x"0079fe93"; -- andi x29, x19, 7
+        temp(13) := x"0049ef13"; -- ori x30, x19, 4
+        temp(14) := x"00000013"; -- addi x0, x0, 0 (NOP)
     return temp;
     end init_rom;   
 
